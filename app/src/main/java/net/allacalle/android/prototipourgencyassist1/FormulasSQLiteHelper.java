bc63892ccf0969a81d5e67de29eefc1efb45bea2
Alfonso Lacalle García <allacalle@gmail.com>
2015-09-16 20:29:11 +0200

    Base de datos muy basica para probar.

---
+++ app/src/main/java/net/allacalle/android/prototipourgencyassist1/FormulasSQLiteHelper.java
@@ -0,0 +1,42 @@
+package net.allacalle.android.prototipourgencyassist1;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class FormulasSQLiteHelper extends SQLiteOpenHelper {
+
+    //Sentencia SQL para crear la tabla de Usuarios
+    String sqlCreateFormula = "CREATE TABLE Formulas ( parametrosFormula TEXT, tipoFormula TEXT, nombreCompleto TEXT, abreviatura TEXT)";
+    String sqlCreatePrueba = "CREATE TABLE Prueba (codigo INTEGER , nombre TEXT)";
+
+
+    public FormulasSQLiteHelper(Context contexto, String nombre,
+                                CursorFactory factory, int version) {
+        super(contexto, nombre, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Se ejecuta la sentencia SQL de creación de la tabla
+        db.execSQL(sqlCreateFormula);
+        db.execSQL(sqlCreatePrueba);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int versionAnterior, int versionNueva) {
+        //NOTA: Por simplicidad del ejemplo aquí utilizamos directamente la opción de
+        //      eliminar la tabla anterior y crearla de nuevo vacía con el nuevo formato.
+        //      Sin embargo lo normal será que haya que migrar datos de la tabla antigua
+        //      a la nueva, por lo que este método debería ser más elaborado.
+
+        //Se elimina la versión anterior de la tabla
+        db.execSQL("DROP TABLE IF EXISTS Formula");
+        db.execSQL("DROP TABLE IF EXISTS Prueba");
+
+
+
+        onCreate(db);
+
+    }
+}
--- app/src/main/java/net/allacalle/android/prototipourgencyassist1/MainActivity.java
+++ app/src/main/java/net/allacalle/android/prototipourgencyassist1/MainActivity.java
@@ -1,17 +1,54 @@
 package net.allacalle.android.prototipourgencyassist1;
 
+
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+    private TextView txtNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Abrimos la base de datos 'DBUsuarios' en modo escritura
+        FormulasSQLiteHelper usdbh =
+                new FormulasSQLiteHelper(this, "DBPrueba", null, 1);
+
+        SQLiteDatabase db = usdbh.getWritableDatabase();
+
+
+        //Si hemos abierto correctamente la base de datos
+
+        if(db != null)
+        {
+            //Insertamos 5 usuarios de ejemplo
+            for(int i=1; i<=5; i++)
+            {
+                //Generamos los datos
+                int codigo = i;
+                String nombre = "Usuario" + i;
+
+                //Insertamos los datos en la tabla Usuarios
+                db.execSQL("INSERT INTO Prueba (codigo, nombre) " +
+                        "VALUES (" + codigo + ", '" + nombre +"')");
+            }
+
+            //Cerramos la base de datos
+
+            db.close();
+            txtNombre = (TextView)findViewById(R.id.TxtNombre);
+            txtNombre.setText("Hola caracola ");
+        }
+
     }
 
     @Override
